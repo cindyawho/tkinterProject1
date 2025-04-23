5cca3700280aa52b2addc97aa11f058d3908d77e
cindyawho <161978639+cindyawho@users.noreply.github.com>
2025-04-22 19:50:57 -0700

    Add changeName method

--- leverapp.py
+++ leverapp.py
@@ -3,7 +3,16 @@ import tkinter as tk
 def buttonClick():
     label.config(text = "WRONG LEVEERRRR")
 
+def changeName():
+    inputName = name.get()
+    label.config(text = f'Pull the lever, {inputName}!')
+
 root = tk.Tk() # creating Tk object which will be our root
+
+name = tk.Entry(root)
+name.pack()
+nameButton = tk.Button(root, text="Change Name", command = changeName)
+nameButton.pack()
 
 root.title("Pull the Lever!")
 

d7d91860a2b1d69211c425ab8292a7ff71f7fce6
Cindy Andrade <cindya3939@gmail.com>
2025-04-22 23:37:31 -0700

    Change font, delete comments of failed attempts lol

--- greeting.py
+++ greeting.py
@@ -14,22 +14,10 @@ bg = tk.PhotoImage(file="space.png")
 background_label = tk.Label(root, image=bg)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-## Failed Attempt trying to import PIL
-# bg = ImageTk.PhotoImage(Image.open("space.png"))
-# root.create_image(0, 0, anchor=NW, image=bg)
-
 # Button Hovering Time!
 style = ttk.Style()
 style.configure("Greet.TButton", background="white", foreground="black", font=("Arial", 10))
 style.map("Greet.TButton", background=[("active", "#E066FF")])
-
-## Failed Attempt using functions bc Mac doesnt like tkinter
-# def onHover(e):
-#     submit["background"] = "#E066FF"
-#     print("onHover!!")
-# def offHover(e):
-#     submit["background"] = "white"
-#     print("Off Hover!")
 
 #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #  ~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~
@@ -62,7 +50,7 @@ nameText = tk.Label(root,
                     text = "What should we call you rebel?",
                     bg = "black",
                     fg = "white",
-                    font = ("Arial", 16)
+                    font = ("Courier New", 16)
                     )
 nameText.pack(pady=(50, 20))
 
@@ -74,7 +62,7 @@ languageText = tk.Label(root,
                         text = "Which language do you prefer?", 
                         bg = "black",
                         fg = "white",
-                        font = ("Arial", 16)
+                        font = ("Courier New", 16)
                         )
 languageText.pack(pady=20)
 
@@ -86,15 +74,13 @@ userLanguage.pack()
 # ----- Submit
 submit = ttk.Button(root, text="Greet Me", command=submitForm, style="Greet.TButton")
 submit.pack(pady=(50, 30))
-# submit.bind("<Enter>", onHover)
-# submit.bind("<Leave>", offHover)
 
 # ----- Output Section
 output = tk.Label(  root, 
                     text = "",
                     bg = "black",
                     fg = "white",
-                    font = ("Arial", 16)
+                    font = ("Courier New", 16)
                   )
 output.pack()
 

b738c856378fd5ffcfc0df09865da70dec46385d
cindyawho <161978639+cindyawho@users.noreply.github.com>
2025-04-29 21:52:13 -0700

    Create checkEntree function and move time.sleep

--- OrderingKiosk/orderingKiosk.py
+++ OrderingKiosk/orderingKiosk.py
@@ -5,6 +5,7 @@ import time # for fun :)
 
 # Main App Information
 root = ThemedTk(theme="radiance")
+root.title("Ordering Kiosk")
 
 class OrderingKiosk():
     def __init__(self, root):
@@ -31,23 +32,47 @@ class OrderingKiosk():
         self.button = ttk.Button(self.root, text="Order", command=self.order)
         self.button.grid(row=5, column=0, columnspan=3, padx=10)
 
+        # Error Handling
         self.nameResultLabel = ttk.Label(self.root, text="")
         self.nameResultLabel.grid(row=6, column=0, columnspan=5, padx=10, pady=10)
+
+        self.foodResultLabel = ttk.Label(self.root, text="")
+        self.foodResultLabel.grid(row=7, column=0, columnspan=5, padx=10, pady=10)
     
     def checkName(self):
         customerName = self.nameEntry.get()
         if customerName == "":
-            time.sleep(2)
             self.nameResultLabel.config(text="Error: Please Enter a Name.", foreground="red")
             return False
         else:
             return customerName
     
+    def checkEntree(self):
+        entreeName = self.foodOption.get()
+        if entreeName == "":
+            self.foodResultLabel.config(text="Error: Please Select an Entree.", foreground="red")
+            return False
+        else:
+            return entreeName
+    
     def order(self):
+        # reset text to erase weird overlapping label texts
+        self.nameResultLabel.config(text="                                                     ")
+        self.foodResultLabel.config(text="                                                     ")
+        self.nameResultLabel.update_idletasks()
+
         print("Customer ordered")
         self.nameResultLabel.config(text="Order Processing...", foreground="blue")
         self.nameResultLabel.update_idletasks()  # force update, otherwise mainloop keeps going and won't show order processing bit
+        time.sleep(2)
         name = self.checkName()
+        entree = self.checkEntree()
+        if name and entree:
+            self.nameResultLabel.config(text=f"Thank you {name}. Your order is coming up shortly!", foreground="green")
+            self.nameResultLabel.update_idletasks()
+            time.sleep(2)
+            self.foodResultLabel.config(text=f"Here's your {entree}!", foreground="green")
+            self.foodResultLabel.update_idletasks()
 
 app = OrderingKiosk(root)
 root.mainloop()

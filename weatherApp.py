79d0ca9717e0adf2bbeef096b053912728b6be9d
cindyawho <161978639+cindyawho@users.noreply.github.com>
2025-04-29 19:40:35 -0700

    Create row 1 with location, input entry, and non-functional button

--- weatherApp.py
+++ weatherApp.py
@@ -6,4 +6,15 @@ root = tk.Tk()
 root.title("PCC Weather App")
 root.geometry("650x250")
 
+# First Row: label, entry box and button
+
+locationLabel = tk.Label(root, text="Location: ", padx=10, pady=5)
+locationLabel.grid(row=0, column=0)
+
+locationEntry = tk.Entry(root)
+locationEntry.grid(row=0, column=1, padx=10)
+
+updateButton = tk.Button(root, text="Find Weather")
+updateButton.grid(row=0, column=2)
+
 root.mainloop()

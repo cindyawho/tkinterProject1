cb1662ef82238ead9ce99c420d99c2e05e781969
cindyawho <161978639+cindyawho@users.noreply.github.com>
2025-05-20 20:03:16 -0700

    Full weather app that calls API

---
+++ updatedWeatherApp/weatherAPI.py
@@ -0,0 +1,6 @@
+import requests # type: ignore # pip install requests.
+import json
+def get_weather(one,two, three):
+    response = requests.get(f'https://api.weather.gov/gridpoints/{three}/{one},{two}/forecast')
+    data = response.json()
+    return data["properties"]['periods'][0]['temperature'], data["properties"]['periods'][0]['detailedForecast'], data["properties"]['periods'][0]['probabilityOfPrecipitation']['value']
---
+++ updatedWeatherApp/weatherDemo.py
@@ -0,0 +1,6 @@
+import weatherUI as wui
+import tkinter as tk
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = wui.WeatherForecast(root)
+    root.mainloop()

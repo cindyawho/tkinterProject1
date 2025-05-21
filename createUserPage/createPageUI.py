c0cc37d26f8e36b6ef76fbe7b9dd06817f9a7b90
cindyawho <161978639+cindyawho@users.noreply.github.com>
2025-05-20 22:07:13 -0700

    Add basic User inputs for HTML page and ignore pycache folder

--- createUserPage/createPageUI.py
+++ createUserPage/createPageUI.py
@@ -1,11 +1,39 @@
 import tkinter as tk
 from tkinter import ttk
 
-class createPageUI:
+# Used https://customtkbuilder.com/
+
+class CreatePage:
     def __init__(self, root):
         self.root = root
-        self.root.title("Create Your Own HTML!")
+        self.root.title("Create Your Own HTML Site!")
         self.root.geometry("700x700")
+        self.root.configure(bg="#B8FFFA")
+        # Styling
+        style = ttk.Style()
+        style.configure("title.TLabel", foreground="black", background="#B8FFFA", font=('Courier New', 18))
+        style.configure("text.TLabel", foreground="black", background="#B8FFFA", font=('Arial', 14))
+        style.configure("note.TLabel", foreground="#FF4255", background="#B8FFFA", font=('Arial', 12))
+        style.configure("TEntry", foreground = "blue")
+
+        # Title
+        self.greetLabel = ttk.Label(self.root, text="Hello! Let's create your very own HTML page!", style="title.TLabel")
+        self.greetLabel.grid(row=0, column=0, columnspan=5, pady=20, padx=20)
+
+        # User Inputs for HTML Page
+        self.nameLabel = ttk.Label(self.root, text="Name: ", style="text.TLabel")
+        self.nameLabel.grid(row=1, column=0, columnspan=2, pady=20, padx=20)
+        self.nameEntry = ttk.Entry(self.root, style="TEntry", width=50)
+        self.nameEntry.grid(row=1, column=1, columnspan=3, pady=20, padx=20)
+
+        self.hobbiesLabel = ttk.Label(self.root, text="Hobbies: ", style="text.TLabel")
+        self.hobbiesLabel.grid(row=2, column=0, columnspan=2, pady=20, padx=20)
+        self.hobbiesEntry = ttk.Entry(self.root, style="TEntry", width=50)
+        self.hobbiesEntry.grid(row=2, column=1, columnspan=3, pady=20, padx=20)
+
+        self.hobbyNoteLabel = ttk.Label(self.root, text="*Note: Please separate each hobby with a comma. ", style="note.TLabel")
+        self.hobbyNoteLabel.grid(row=3, column=0, columnspan=4, padx=20)
+
 
     def writeFile():
         with open('index.html', 'r+') as f:

1c48c2aa4a8c565165fa0b1d75550e1da9a10429
cindyawho <161978639+cindyawho@users.noreply.github.com>
2025-05-20 22:38:45 -0700

    Add some CSS user input options like checkbox for default styling and background color picker

--- createUserPage/createPageUI.py
+++ createUserPage/createPageUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.colorchooser import askcolor
 
 # Used https://customtkbuilder.com/
 
@@ -16,7 +17,7 @@ class CreatePage:
         style.configure("note.TLabel", foreground="#FF4255", background="#B8FFFA", font=('Arial', 12))
         style.configure("TEntry", foreground = "blue")
 
-        # Title
+        # HTML Title
         self.greetLabel = ttk.Label(self.root, text="Hello! Let's create your very own HTML page!", style="title.TLabel")
         self.greetLabel.grid(row=0, column=0, columnspan=5, pady=20, padx=20)
 
@@ -34,6 +35,41 @@ class CreatePage:
         self.hobbyNoteLabel = ttk.Label(self.root, text="*Note: Please separate each hobby with a comma. ", style="note.TLabel")
         self.hobbyNoteLabel.grid(row=3, column=0, columnspan=4, padx=20)
 
+         # CSS Title
+        self.greetLabel = ttk.Label(self.root, text="Time to Style Your Site! If you prefer, \nyou can use our default styling.", style="title.TLabel")
+        self.greetLabel.grid(row=4, column=0, columnspan=5, pady=(75, 20), padx=20)
+        # CSS Use Default
+        self.agree = tk.BooleanVar(self.root, True)
+        self.checkbox = ttk.Checkbutton(
+            self.root,
+            text='Use Default Styling - If checked, you can skip the next CSS inputs and default styling will be used.',
+            command=self.checkCSSChoice,
+            variable=self.agree
+        )
+        self.checkbox.grid(row=5, column=0, columnspan=5, pady=20, padx=20)
+
+        # User Inputs for CSS
+        self.bgColorLabel = ttk.Label(self.root, text="Background Label: ", style="text.TLabel")
+        self.bgColorLabel.grid(row=6, column=0, columnspan=2, pady=20, padx=20)
+        self.bgColorButton = ttk.Button(self.root, text='Select a BackgroundColor', command=self.changeColor)
+        self.bgColorButton.grid(row=6, column=1, columnspan=3, pady=20, padx=20)
+        self.colorLabel = ttk.Label(self.root, text=".   color   .", style="text.TLabel")
+        self.colorLabel.grid(row=6, column=3, columnspan=2, pady=20, padx=20)
+
+    # ~~~~~~~~~~~~ Form Functions ~~~~~~~~~~~~~~~
+    # color picker
+    def changeColor(self):
+        colors = askcolor(title="Tkinter Color Chooser")
+        # print(colors)
+        self.colorLabel.configure(background=colors[1])
+
+    # default or user Styling
+    def checkCSSChoice(self):
+        # print(self.agree.get())
+        if self.agree.get():
+            print("Use Default Styling")
+        else:
+            print("User will input their CSS Choices")
 
     def writeFile():
         with open('index.html', 'r+') as f:
